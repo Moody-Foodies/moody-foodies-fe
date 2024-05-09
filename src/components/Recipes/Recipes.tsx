4e2f071ec73100752cdf5d9a10165f1ac7c7447c
Erin Kelley <erin.kelley01@gmail.com>
2024-05-09 15:45:54 -0600

    Add react slick library

--- src/components/Recipes/Recipes.tsx
+++ src/components/Recipes/Recipes.tsx
@@ -1,13 +1,34 @@
 // import { useState } from 'react'
-import Carousel from '../Carousel/Carousel'
-import './Recipes.css'
+// import Carousel from '../Carousel/Carousel'
+
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './Recipes.css';
 
 function Recipes() {
-
+  let test = [1, 2, 3, 4, 5]
+  let settings = {
+    dots: true, 
+    infinite: true, 
+    speed: 500, 
+    slidesToShow: 1, 
+    slidesToScroll: 1
+  }
   return (
     <>
       <p>Recipes Component</p>
-      <Carousel />
+      <Slider {...settings}>
+    {test.map(t => {
+      return (
+        <section className='recipe-carousel'>
+        <h2>{t}</h2>
+      </section>
+      )
+    })}
+      
+ 
+      </Slider> 
     </>
   )
 }

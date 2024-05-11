8a5fe291e0456cae1cc77f4f91846fda973215f7
Erin Kelley <erin.kelley01@gmail.com>
2024-05-11 13:39:12 -0600

    Add input field to landing page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,16 +9,22 @@ function Home() {
 const navigate = useNavigate()
 console.log(value)
   return (
+    <main>
+    <h1>Brain Food</h1>
+    <h2>How are you feeling today?</h2>
       <section className='slider-container'>
       <Box sx={{ width: 500 }}>
         <Slider defaultValue={2} step={1} marks min={0} max={5} aria-label="Default" valueLabelDisplay="auto" onChange={(event) => setValue(event.target.value)}/>
       </Box>
       </section>
+      <h2>I have {<input type='number' />} minutes to cook.</h2>
+      <button onClick={() => navigate('/recipes')}>Let's cook!</button>
+     </main> 
     )
     // <>
     //   <p>Home Component</p>
       
-    //   <button onClick={() => navigate('/recipes')}>Let's cook!</button>
+      
     // </>
 
 }

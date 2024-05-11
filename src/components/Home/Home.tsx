5530d5fd58763c5b3e47393430690f8b8f62471d
Erin Kelley <erin.kelley01@gmail.com>
2024-05-11 13:27:00 -0600

    Add event to log value of slider

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,26 @@
-// import { useState } from 'react'
+import { useState } from 'react';
 import './Home.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 function Home() {
+  const [value, setValue] = useState(0)
 const navigate = useNavigate()
+console.log(value)
   return (
-    <>
-      <p>Home Component</p>
-      <button onClick={() => navigate('/recipes')}>Let's cook!</button>
-    </>
-  )
+      <section className='slider-container'>
+      <Box sx={{ width: 500 }}>
+        <Slider defaultValue={2} step={1} marks min={0} max={5} aria-label="Default" valueLabelDisplay="auto" onChange={(event) => setValue(event.target.value)}/>
+      </Box>
+      </section>
+    )
+    // <>
+    //   <p>Home Component</p>
+      
+    //   <button onClick={() => navigate('/recipes')}>Let's cook!</button>
+    // </>
+
 }
 
 export default Home

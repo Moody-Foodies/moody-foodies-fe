274db15c9792956c7237a529f70601993ab60fd3
Erin Kelley <erin.kelley01@gmail.com>
2024-05-11 13:52:47 -0600

    Change font style

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,19 +5,21 @@ import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
 
 function Home() {
-  const [value, setValue] = useState(0)
+  const [moodValue, setMoodValue] = useState(0)
+  const [timeValue, setTimeValue] = useState(0)
 const navigate = useNavigate()
-console.log(value)
+console.log(moodValue)
+console.log(timeValue)
   return (
-    <main>
+    <main className='landing-page'>
     <h1>Brain Food</h1>
     <h2>How are you feeling today?</h2>
       <section className='slider-container'>
       <Box sx={{ width: 500 }}>
-        <Slider defaultValue={2} step={1} marks min={0} max={5} aria-label="Default" valueLabelDisplay="auto" onChange={(event) => setValue(event.target.value)}/>
+        <Slider step={1} marks min={0} max={5} aria-label="Default" valueLabelDisplay="auto" onChange={(event) => setMoodValue(event.target.value)}/>
       </Box>
       </section>
-      <h2>I have {<input type='number' />} minutes to cook.</h2>
+      <h2>I have {<input type='number' value={timeValue} onChange={(event) => setTimeValue(event.target.value)}/>} minutes to cook.</h2>
       <button onClick={() => navigate('/recipes')}>Let's cook!</button>
      </main> 
     )

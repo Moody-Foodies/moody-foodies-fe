bbeecd86a10130fb3369dac28f914278bf4d75e6
Laura Long <lauralonggone@gmail.com>
2024-05-13 16:21:35 -0600

    Update Dashboard component and add separate Carosel comp to DRY up code

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,35 @@
 // import { useState } from 'react'
 import './Dashboard.css'
+import { useNavigate } from 'react-router-dom'
+import affirmations from '../../Quotes/quotes'
+import Carousel from '../Carousel/Carousel'
 
-function Dashboard() {
+type Affirmation = {
+    quote: string
+    }
 
-  return (
-    <>
-      <p>Dashboard component</p>
-    </>
-  )
-}
-
-export default Dashboard
+    export default function Dashboard() {
+        const [quote, setQuote] = useState('')
+        const navigate = useNavigate()
+      
+        function getRandomAffirmation(affirmations: Affirmation[]) {
+           let randomQuote = affirmations[Math.floor(Math.random() * affirmations.length)]
+           setQuote(randomQuote.quote)
+        }
+      
+        useEffect(() => {
+          getRandomAffirmation(affirmations)
+        }, [])
+      
+        return (
+          <section className='container'>
+          <header className='dashboard-container'>
+            <h2 className='back-arrow' onClick={() => navigate('/')}>⬅</h2>
+            <h2 className='dashboard'>Mood Board</h2>
+          </header>
+          <h3 className='average-mood-score'>Average mood score: 7.5</h3>
+          <h3 className='affirmation'>{quote}</h3>
+          <Carousel />
+        </section>
+        )
+      }

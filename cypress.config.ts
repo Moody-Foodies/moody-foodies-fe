3d120c7ea69a028b00582ee3a75a6de704be537d
Laura Long <lauralonggone@gmail.com>
2024-05-15 19:37:50 -0600

    fix: add start script to package.json and ensure server readiness for Cypress, update cypress baseUrl to postman mock server

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,7 +12,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://7a97657d-b4dd-468a-960b-563f46161622.mock.pstmn.io',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
